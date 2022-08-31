e7def9adfdd6f0d56fb3c53c9e1d340d94dbaf2b
Darlan Tane <darlan.tane@gmail.com>
2022-08-31 21:00:18 +0200

    prime and even numbers

--- variables/prime.py
+++ variables/prime.py
@@ -1,9 +1,16 @@
 user_input = input('enter a number')
 user_input = int(user_input)
 
-
-for i in range(2, user_input-1):
-    if user_input % i == 0:
-        print('this number isn t prime')
+prime_flag = 0
+if user_input > 1 :
+    for i in range(2, user_input - 1):
+        if user_input % i == 0:
+            prime_flag = 1
+            break
+    if prime_flag == 0 :
+        print("this number is prime")
     else:
-        print('this number is prime')
+        print("this number isn t prime")
+
+else:
+    print('this number isn t prime')
