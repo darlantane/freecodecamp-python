4f667334e62a59b78998a53dc5990d9e9eb7bc1f
Darlan Tane <darlan.tane@gmail.com>
2022-09-14 23:35:11 +0200

    Polygon_area_calculator

---
+++ shape_calculator/shape_calculator.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+
+    def set_width(self, w):
+        self.width = w
+
+    def set_height(self, h):
+        self.height = h
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
