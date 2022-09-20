627745818edf2e410b14dc792f7abad775bf093c
Darlan Tane <darlan.tane@gmail.com>
2022-09-20 22:44:01 +0200

    fix budget app

--- budget app/budget.py
+++ budget app/budget.py
@@ -4,45 +4,45 @@ class Category:
         self.ledger = []
         self.__balance = 0.0
 
-def __repr__(self):
-    header = self.description.center(30, "*") + "\n"
-    ledger = ""
-    for item in self.ledger:
-        # format description and amount
-        line_description = "{:<23}".format(item["description"])
-        line_amount = "{:>7.2f}".format(item["amount"])
+    def __repr__(self):
+        header = self.description.center(30, "*") + "\n"
+        ledger = ""
+        for item in self.ledger:
+            # format description and amount
+            line_description = "{:<23}".format(item["description"])
+            line_amount = "{:>7.2f}".format(item["amount"])
 
-        ledger += "{}{}\n".format(line_description[:23], line_amount[:7])
-    total = "Total: {:.2f}".format(self.__balance)
-    return header + ledger + total
+            ledger += "{}{}\n".format(line_description[:23], line_amount[:7])
+        total = "Total: {:.2f}".format(self.__balance)
+        return header + ledger + total
 
-def deposit(self, amount, description=""):
-    self.ledger.append({"amount": amount, "description": description})
-    self.__balance += amount
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+        self.__balance += amount
 
-def withdraw(self, amount, description=""):
-    if self.__balance - amount >= 0:
-        self.ledger.append({"amount": -1 * amount, "description": description})
-        self.__balance -= amount
-        return True
-    else:
-        return False
+    def withdraw(self, amount, description=""):
+        if self.__balance - amount >= 0:
+            self.ledger.append({"amount": -1 * amount, "description": description})
+            self.__balance -= amount
+            return True
+        else:
+            return False
 
-def get_balance(self):
-    return self.__balance
+    def get_balance(self):
+        return self.__balance
 
-def transfer(self, amount, category_instance):
-    if self.withdraw(amount, "Transfer to {}".format(category_instance.description)):
-        category_instance.deposit(amount, "Transfer from {}".format(self.description))
-        return True
-    else:
-        return False
+    def transfer(self, amount, category_instance):
+        if self.withdraw(amount, "Transfer to {}".format(category_instance.description)):
+            category_instance.deposit(amount, "Transfer from {}".format(self.description))
+            return True
+        else:
+            return False
 
-def check_funds(self, amount):
-    if self.__balance >= amount:
-        return True
-    else:
-        return False
+    def check_funds(self, amount):
+        if self.__balance >= amount:
+            return True
+        else:
+            return False
 
 
 def create_spend_chart(categories):
