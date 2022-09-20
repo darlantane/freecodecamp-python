2919c811bc86c45e28a8507617b3243eed9da602
Darlan Tane <darlan.tane@gmail.com>
2022-09-20 07:34:20 +0200

    fix shape_area

--- polygon_area/main.py
+++ polygon_area/main.py
@@ -1,8 +1,8 @@
-import shape_calculator
+import shape_area
 from unittest import main
 
 
-rect = shape_calculator.Rectangle(5, 10)
+rect = shape_area.Rectangle(5, 10)
 print(rect.get_area())
 rect.set_width(3)
 print(rect.get_perimeter())
